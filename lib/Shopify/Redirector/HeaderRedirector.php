4ed5f12412d3a81e0f608826f2015f7ca3db9b46
Erwin Kooi <ek@cdyweb.com>
2015-05-29 22:07:52 -0400

    Allow redirecting without having to create a Redirector object first

--- lib/Shopify/Redirector/HeaderRedirector.php
+++ lib/Shopify/Redirector/HeaderRedirector.php
@@ -7,10 +7,16 @@ class HeaderRedirector implements \Shopify\Redirector
 
     public function redirect($uri)
     {
+        self::go($uri);
+    }
 
+    public static function go($uri)
+    {
         header('Location: ' . $uri);
         exit(0);
 
     }
+
+
 
 }
